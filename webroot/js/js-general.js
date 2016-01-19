59aea666d04fc7062bf395e615ae6c0b8aaec3ab
marcelito <mdpilone@gmail.com>
2016-01-19 09:27:47 -0300

    checked inactivo

--- webroot/js/js-general.js
+++ webroot/js/js-general.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
         $('#footer').removeClass('footer-fixeado');
         $('#footer').addClass('footer-relativo');
     }
+    $('#PersonaLegajoEstadoId1').prop('checked', true);
+    
+    if($("#PersonaLegajoEstadoId2").is(':checked')) { 
+        $('#PersonaLegajoEstadoId1').prop('checked', false);
+    }
     
     $('.combobox').combobox();
   
